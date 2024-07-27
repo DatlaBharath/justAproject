16929978a09c7c59b2ea2cc023aaa232737a8c6e
DatlaBharath <datlabharath92@gmail.com>
2024-07-27 16:59:04 +0530

    fist spboot to git

---
+++ src/test/java/com/git/jap/JustAprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.jap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustAprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
